2d711f87b0f05daf094697fbd98ad164e177255c
chatc <zys_819@126.com>
2019-05-29 14:31:55 +0800

    problem2

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+module.exports = function main(number) 
+{
+    var line = new Array(3)
+    line[0] = '._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.'
+    line[1] = '|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|'
+    line[2] = '|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|'
+    var ans = new Array('','','')
+    
+    for (var i=0; i < number.length; i++)
+    {
+        cur_number = parseInt(number[i])
+        for(var j=0; j<3; j++)
+        {
+            ans[j] += line[j].substr(cur_number*6, 3) + (i==number.length-1?'\n':' ')
+        }
+    }
+    return ans[0]+ans[1]+ans[2];
+  };
